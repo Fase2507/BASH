15d896227d0ecea9148d4917909838e4ae70d489
Fase2507 <fase2507@gmail.com>
2024-02-06 22:43:00 +0300

    small

---
+++ PaswrdGnrtorv2.1.sh
@@ -0,0 +1,23 @@
+ #! /usr/bin/bash
+ echo "Please enter the word that u wanna add first into your pswrd"
+ read var1 
+ echo "Please enter the word that u wanna add last into ypur pswrd"
+ read var
+ echo "Please enter the length of the pssword"
+ read PASS_LENGTH
+ echo "Please enter the number of the pssword samples"
+ read n
+
+ pswrdgnrtr() {
+ for p in $(seq 1 $n);
+ do
+
+  echo $var1 && openssl rand -base64 48 | cut -c1-$PASS_LENGTH && echo $var '||'
+
+ done
+
+}
+VAR=$(pswrdgnrtr;)
+VAR="${VAR%x}"
+echo ${VAR}
+#echo $var${VAR}$var1 
